83edbcb04861faadad075f3b53643a51a6ab041b
James Armbrust <james.armbrust@gmail.com>
2019-10-30 08:00:08 -0600

    mispelling

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
           <div className="intro-text">
             <h3>Welcome to jamesarmbrust.dev!</h3>
             <br />
-            <p>This site is only a few days old right now, so still a WIP, but stay tooned for more content!</p>
+            <p>This site is only a few days old right now, so still a WIP, but stay tuned for more content!</p>
             <p>For now, there are a few pages above of misc work and information about me.</p>
             <br />
             <p>The sourcecode for this site is located <a target="_blank" rel="noopener noreferrer" href="https://github.com/jarmbrust/homepage-dev1">here</a>.</p>

7ddd1e492eb432cf61364c0effdb8b05cbf8da79
James Armbrust <james.armbrust@gmail.com>
2020-02-02 14:56:58 -0700

    added info page

---
+++ src/Oosular/Information.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Oosular.scss';
+import {
+  Container
+} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Information = () => {
+  return (
+    <div className="oosular-background">
+      <Container className="col-md-6">
+        Character Creation and general information will go here.
+      </Container>
+    </div>
+  )
+}
+
+export default Information;
